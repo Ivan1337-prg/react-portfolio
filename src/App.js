b2353c4584e0ff3b24efc8b5340e8b786f8cc760
Taras Glushko <tarasglushko2004@gmail.com>
2024-10-09 21:46:51 -0500

    Update website files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
-import './App.scss'
+import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <Router basename="/">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          {/* Fallback route for unmatched paths */}
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+
+/*import './App.scss'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home';
@@ -21,4 +47,4 @@ function App() {
 }
 
 export default App;
-
+*/
